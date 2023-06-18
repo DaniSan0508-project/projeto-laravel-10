1cb6271756715b2ac2f2e2811d9788b81e00f586
Daniel Santos <danielsantos@sysfar.com.br>
2023-06-18 19:17:47 -0300

    implementado crud clientes

--- resources/views/components/navigate.blade.php
+++ resources/views/components/navigate.blade.php
@@ -1,30 +1,30 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-    <div class="position-sticky pt-3 sidebar-sticky">
+  <div class="position-sticky pt-3 sidebar-sticky">
       <ul class="nav flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">
-            <span data-feather="home" class="align-text-bottom"></span>
-            Dashboard
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span data-feather="file" class="align-text-bottom"></span>
-            Vendas
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('produto.index') }}">
-            <span data-feather="shopping-cart" class="align-text-bottom"></span>
-            Produtos
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('cliente.index') }}">
-            <span data-feather="users" class="align-text-bottom"></span>
-            Clientes
-          </a>
-        </li>
+          <li class="nav-item">
+              <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" aria-current="page" href="/">
+                  <span data-feather="home" class="align-text-bottom"></span>
+                  Dashboard
+              </a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link {{ request()->is('vendas') ? 'active' : '' }}" href="/vendas">
+                  <span data-feather="file" class="align-text-bottom"></span>
+                  Vendas
+              </a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link {{ request()->is('produtos*') ? 'active' : '' }}" href="{{ route('produto.index') }}">
+                  <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                  Produtos
+              </a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link {{ request()->is('clientes*') ? 'active' : '' }}" href="{{ route('cliente.index') }}">
+                  <span data-feather="users" class="align-text-bottom"></span>
+                  Clientes
+              </a>
+          </li>
       </ul>
-    </div>
-  </nav>
+  </div>
+</nav>
